8b5a43a44afbbf1d53f40281f1a8e4be024beed5
Boualleg Hibat arrahman <hibaboub88@gmail.com>
2025-02-18 08:20:44 +0100

    Homework.c

--- Homework.c
+++ Homework.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
-int odd_arr(int arr[],int x){
+int odd_arr(int arr[],int x,int size){
 int count=0;
-if(x > 5){
+if(x >size){
 return 0;
 }
 if(arr[x]%2 !=0){
 count++;
 }
-return count + odd_arr(arr,x+1);
+return count + odd_arr(arr,x+1,size);
 }
 
-int even_arr(int arr[],int x){
+int even_arr(int arr[],int x,int size){
 int count=0;
-if(x > 5){
+if(x > size){
 return 0;
 }
 if(arr[x]%2 == 0){
 count++;
 }
-return count + even_arr(arr,x+1);
+return count + even_arr(arr,x+1,size);
 }
 
 
 int main(){
 
 int arr[6]={1,2,3,4,5,6};
-
-	printf("the number of even number is %d , the number of odd numbers is %d ",even_arr(arr,0),odd_arr(arr,0));
+int size =5;
+	printf("the number of even number is %d , the number of odd numbers is %d ",even_arr(arr,0,5),odd_arr(arr,0,5));
 }
